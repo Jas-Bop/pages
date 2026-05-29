381d853130fb3ad975d503a0549d90ff8a2fd4c8
rishab <rishab@shyamal.com>
2026-05-29 09:20:28 -0700

    added our game level background

--- _projects/games/mansionGame/levels/mansionLevel4.js
+++ _projects/games/mansionGame/levels/mansionLevel4.js
@@ -18,7 +18,7 @@ class MansionLevel4 {
         const image_data_background = {
             name: 'background',
             greeting: "Wave Defense! Defeat all enemies to proceed!",
-            src: path + "/images/projects/mansionGame/lvl4.png",
+            src: path + "/images/projects/mansionGame/level4.png",
             pixels: { height: 1600, width: 1600 }
         };
 
